3a78075a2316d9239542b341525b8f52dc570178
alber <trfytfty>
2024-04-14 21:53:01 +0200

    adapt code to BASELINE 1 task and create scripts for training

---
+++ script_RESNET18_BASELINE_FOOD101.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+python3 src/main_incremental.py \
+    --approach seed \
+    --gmms 1 \
+    --max-experts 1 \
+    --use-multivariate \
+    --nepochs 50 \
+    --batch-size 128 \
+    --datasets food101 \
+    --num-tasks 1 \
+    --lr 0.05 \
+    --weight-decay 5e-4 \
+    --clipping 1 \
+    --alpha 0.99 \
+    --use-test-as-val \
+    --network resnet18 \
+    --pretrained \
+    --momentum 0.9 \
+    --exp-name exp_BASELINE_res18_food101
